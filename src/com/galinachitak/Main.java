743c5563154ce528f69473d05205ffda302fe30f
Galina Chitak <gala199@list.ru>
2022-01-26 20:06:33 +0300

    Update main

--- src/com/galinachitak/Main.java
+++ src/com/galinachitak/Main.java
@@ -1,8 +1,23 @@
 package com.galinachitak;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите сумму вклада: ");
+        double principal = input.nextDouble();
+
+        System.out.print("Ставка  в месяц 1.5% \n");
+        double rate = 1.5;
+
+        System.out.print("Введите срок (лет): ");
+        double time = input.nextDouble();
+
+        double interest = principal * (Math.pow((1 + rate / 100), (time * 12)));
+        System.out.println("Общая сумма: " + interest);
+        input.close();
+
     }
 }
